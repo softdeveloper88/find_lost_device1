2fbf8823e136d83f43ad28eaa741c792906a0501
hassan <softdeveloper88@gmail.com>
2021-08-27 10:32:24 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/hura/find_lost_device1/MainActivity.kt
@@ -0,0 +1,423 @@
+package com.hura.find_lost_device1
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.*
+import android.app.admin.DevicePolicyManager
+import android.content.*
+import android.content.pm.PackageManager
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.*
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import com.hura.find_lost_device1.R
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.OnFailureListener
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+class MainActivity : FlutterActivity() {
+    val RESULT_ENABLE = 1
+    var deviceManger: DevicePolicyManager? = null
+    var activityManager: ActivityManager? = null
+    var compName: ComponentName? = null
+    var mLocationService: LocationService = LocationService()
+    lateinit var mServiceIntent: Intent
+    lateinit var mActivity: Activity
+    var sensorScreen = false
+    private val service: BatteryService? = null
+    private val CHANNEL = "flutter.native/helper"
+    lateinit var editor: SharedPreferences.Editor
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        deviceManger = getSystemService(
+            Context.DEVICE_POLICY_SERVICE
+        ) as DevicePolicyManager
+        activityManager = getSystemService(
+            Context.ACTIVITY_SERVICE
+        ) as ActivityManager
+        compName = ComponentName(this, MyAdmin::class.java)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler {
+                // Note: this method is invoked on the main thread.
+                    call, result ->
+                when (call.method) {
+                    "uninstallApp" -> {
+                        val greetings: String = uninstallApp()
+                        result.success(greetings)
+                    }
+                    "lockScreen" -> {
+                        val lockScreen: Boolean = lock()
+                        result.success(lockScreen)
+                    }
+                    "disable" -> {
+                        sensorScreen = true
+//                        result.success(sensorScreen)
+
+                    }
+                    "batteryStart" -> {
+                        if (service == null) {
+                            Toast.makeText(this@MainActivity, "start", Toast.LENGTH_SHORT).show()
+                            // start service
+                            val i = Intent(this, BatteryService::class.java)
+                            startService(i)
+                        }
+                    }
+                    "batteryStop" -> {
+                        if (service == null) {
+                            Toast.makeText(this@MainActivity, "stop", Toast.LENGTH_SHORT).show()
+                            val i = Intent(this, BatteryService::class.java)
+                            stopService(i)
+                        }
+                    }
+                    "eraseData" -> {
+                        val active = call.argument<Boolean>("active")
+                        editor.putBoolean("active", active!!)
+                        editor.apply()
+
+                        val isDeleted: Boolean = deleteAllData()
+                    }
+
+                }
+            }
+
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        try {
+            when (requestCode) {
+                RESULT_ENABLE -> {
+                    if (resultCode == Activity.RESULT_OK) {
+                        Log.i("DeviceAdminSample", "Admin enabled!")
+                    } else {
+                        Log.i("DeviceAdminSample", "Admin enable FAILED!")
+                    }
+                    return
+                }
+                1001 -> {
+                    if (Build.VERSION.SDK_INT >= VERSION_CODES.M) {
+                        if (ActivityCompat.checkSelfPermission(
+                                context,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            ) == PackageManager.PERMISSION_GRANTED ||
+                            ActivityCompat.checkSelfPermission(
+                                context,
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            ) == PackageManager.PERMISSION_GRANTED
+                            || ActivityCompat.checkSelfPermission(
+                                context,
+                                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                            ) == PackageManager.PERMISSION_GRANTED
+                        ) {
+
+                        }
+                    }
+                }
+                2296 -> {
+                    if (SDK_INT >= Build.VERSION_CODES.R) {
+                        if (Environment.isExternalStorageManager()) {
+                            // perform action when allow permission success
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Allow permission for storage access!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+            super.onActivityResult(requestCode, resultCode, data)
+        } catch (e: java.lang.RuntimeException) {
+
+        }
+    }
+
+
+    private fun uninstallApp(): String {
+        val intent = Intent(Intent.ACTION_DELETE)
+        intent.data = Uri.parse("package:" + this.packageName)
+        startActivity(intent)
+        return "Hello testing"
+
+    }
+
+
+    private fun lock(): Boolean {
+        val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+        intent.putExtra(
+            DevicePolicyManager.EXTRA_DEVICE_ADMIN,
+            compName
+        )
+        intent.putExtra(
+            DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+            "Additional text explaining why this needs to be added."
+        )
+
+        startActivityForResult(intent, RESULT_ENABLE)
+        val active = deviceManger!!.isAdminActive(compName!!)
+        if (active) {
+            deviceManger!!.lockNow()
+        }
+        val pm: PowerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+
+        if (pm.isScreenOn) {
+            val policy: DevicePolicyManager =
+                getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+            try {
+                policy.lockNow()
+            } catch (ex: SecurityException) {
+                Toast.makeText(
+                    this,
+                    "Must be enable device administrator",
+                    Toast.LENGTH_LONG
+                ).show()
+                val admin = ComponentName(context, MyAdmin::class.java)
+                val intent: Intent = Intent(
+                    DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN
+                ).putExtra(
+                    DevicePolicyManager.EXTRA_DEVICE_ADMIN, admin
+                )
+                context.startActivity(intent)
+            }
+        }
+
+        return true
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        FlutterMain.startInitialization(this)
+        editor = getSharedPreferences("preferences", MODE_PRIVATE).edit()
+        mLocationService = LocationService()
+        mServiceIntent = Intent(context, mLocationService.javaClass)
+        mLocationService = LocationService()
+        mActivity = this@MainActivity
+//        checkRunTimePermission()
+        if (!Util.isLocationEnabledOrNot(this)) {
+            dialogGpsEnable()
+        }
+        mLocationService = LocationService()
+        mServiceIntent = Intent(this, mLocationService.javaClass)
+        if (!Util.isMyServiceRunning(mLocationService.javaClass, mActivity)) {
+            startService(mServiceIntent)
+            Toast.makeText(
+                mActivity,
+                getString(R.string.service_start_successfully),
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                mActivity,
+                getString(R.string.service_already_running),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
+//        val intent = Intent()
+//        val packageName = context.packageName
+//        val pm = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+//        if (pm.isIgnoringBatteryOptimizations(packageName)) intent.action =
+//            Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS else {
+//            intent.action = Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
+//            intent.data = Uri.parse("package:$packageName")
+//        }
+//        context.startActivity(intent)
+
+
+//        val front_translucent = Intent(this, NotificationService::class.java)
+//        startService(front_translucent)
+//        val intent = Intent(
+//            this@MainActivity,
+//            MyServices::class.java
+//        )
+//        intent.action = MyServices.ACTION_START_FOREGROUND_SERVICE
+//        startService(intent)
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val channelId = getString(R.string.default_notification_channel_id)
+            val channelName = getString(R.string.default_notification_channel_name)
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(
+                NotificationChannel(
+                    channelId,
+                    channelName, NotificationManager.IMPORTANCE_HIGH
+                )
+            )
+        }
+
+        intent.extras?.let {
+            for (key in it.keySet()) {
+                val value = intent.extras?.get(key)
+                Log.d("TAG", "Key: $key Value: $value")
+            }
+        }
+
+    }
+
+
+
+    fun checkRunTimePermission() {
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+
+            } else {
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    10
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 10) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                        (context as Activity),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                ) {
+                    // If User Checked 'Don't Show Again' checkbox for runtime permission, then navigate user to Settings
+                    val dialog: AlertDialog.Builder = AlertDialog.Builder(context)
+                    dialog.setTitle("Permission Required")
+                    dialog.setCancelable(false)
+                    dialog.setMessage("You have to Allow permission to access user location")
+                    dialog.setPositiveButton(
+                        "Settings"
+                    ) { dialog, which ->
+                        val i = Intent(
+                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts(
+                                "package",
+                                context.packageName, null
+                            )
+                        )
+                        //i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivityForResult(i, 1001)
+                    }
+                    val alertDialog = dialog.create()
+                    alertDialog.show()
+                }
+                //code for deny
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (sensorScreen) {
+            val activityManager = applicationContext
+                .getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+            activityManager.moveTaskToFront(taskId, 0)
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.O) {
+            if (sensorScreen) {
+                if (!hasFocus) {
+                    val closeDialog = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
+                    sendBroadcast(closeDialog)
+                    // Method that handles loss of window focus
+
+                }
+            }
+        }
+    }
+
+
+    fun deleteAllData(): Boolean {
+        val dataDir = File(Environment.getExternalStorageDirectory().absolutePath)
+        if (dataDir.length() > 0) {
+            ListDir(dataDir)
+        }
+        val file = File("/storage/emulated/0/App_Name")
+        val deleted: Boolean = file.delete()
+        //just to scan an update for storage to be notified
+
+//just to scan an update for storage to be notified
+        MediaScannerConnection.scanFile(
+            context, arrayOf<String>(file.absolutePath),
+            null
+        ) { path, uri ->
+            //just to scan an update for gallery to be notified of a new image.
+        }
+
+        return true
+    }
+
+    fun ListDir(f: File) {
+        val files = f.listFiles()
+        val fileList: ArrayList<String> = ArrayList()
+        for (file in files) {
+            fileList.add(file.path)
+            Log.d("Listfile::", file.path)
+        }
+
+    }
+
+
+    private fun dialogGpsEnable() {
+
+        val requestLocation = LocationRequest.create()
+        requestLocation.interval = 10000
+        requestLocation.fastestInterval = 5000
+        requestLocation.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        val mBuilder = LocationSettingsRequest.Builder().addLocationRequest(requestLocation)
+        val settingsClient = LocationServices.getSettingsClient(context)
+        val task: com.google.android.gms.tasks.Task<LocationSettingsResponse>? =
+            settingsClient.checkLocationSettings(mBuilder.build())
+        task!!.addOnSuccessListener(
+            activity
+        ) {
+
+        }
+        task.addOnFailureListener(activity, OnFailureListener { e ->
+            if (e is ResolvableApiException) {
+                val resolvable: ResolvableApiException = e
+                try {
+                    resolvable.startResolutionForResult(activity, 51)
+                } catch (e1: IntentSender.SendIntentException) {
+                    e1.printStackTrace()
+                }
+            }
+        })
+    }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,327 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:ui';
+
+import 'package:background_location/background_location.dart';
+import 'package:find_lost_device1/screens/dashboard.dart';
+import 'package:find_lost_device1/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flashlight/flashlight.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:shared_value/shared_value.dart';
+import 'package:vibration/vibration.dart';
+
+//initializeService
+// Future<dynamic> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   // If you're going to use other Firebase services in the background, such as Firestore,
+//   // make sure you call `initializeApp` before using other Firebase services.
+//   await Firebase.initializeApp();
+//   WidgetsFlutterBinding.ensureInitialized();
+//   getCurrentLocation();
+//   // responseLockApp();
+//   // states.currentState.responseLockApp();
+//   print('Handling a background message ${message.data['title']}');
+//
+// }
+//
+// Future<void> responseLockApp() async {
+//   bool res;
+//   try {
+//     const platform1 = const MethodChannel('flutter.native/helper');
+//
+//     final bool result = await platform1.invokeMethod("lockScreen");
+//     res = result;
+//   } on PlatformException catch (e) {
+//     var bools = "Failed to Invoke: '${e.message}'.";
+//   }
+//
+// }
+Future<dynamic> _throwGetMessage(RemoteMessage message) async {
+  print("PUSH RECEIVED");
+  // getCurrentLocation();
+}
+
+void getCurrentLocation() {
+  BackgroundLocation.startLocationService();
+  BackgroundLocation().getCurrentLocation().then((location) async {
+    print('This is current Location ' + location.toMap().toString());
+    BackgroundLocation().getCurrentLocation().then((location) {
+      print('This is current Location ' + location.toMap().toString());
+    });
+    await BackgroundLocation.setAndroidNotification(
+      title: 'Background service is running',
+      message: 'Background location in progress',
+      icon: '@mipmap/ic_launcher',
+    );
+    await BackgroundLocation.setAndroidConfiguration(1000);
+    await BackgroundLocation.startLocationService(distanceFilter: 20);
+    BackgroundLocation.getLocationUpdates((location) {
+      var time =
+          DateTime.fromMillisecondsSinceEpoch(location.time.toInt()).toString();
+      print("Time:: $time");
+      print("Latitude:${location.latitude} Longitude: ${location.longitude}");
+
+      //   latitude = location.latitude.toString();
+      //   longitude = location.longitude.toString();
+      //   accuracy = location.accuracy.toString();
+      //   altitude = location.altitude.toString();
+      //   bearing = location.bearing.toString();
+      //   speed = location.speed.toString();
+      //   time = DateTime.fromMillisecondsSinceEpoch(
+      //       location.time!.toInt())
+      //       .toString();
+      // });
+      // print('''\n
+      //                   Latitude:  $latitude
+      //                   Longitude: $longitude
+      //                   Altitude: $altitude
+      //                   Accuracy: $accuracy
+      //                   Bearing:  $bearing
+      //                   Speed: $speed
+      //                   Time: $time
+      //                 ''');
+    });
+  });
+}
+
+/// Create a [AndroidNotificationChannel] for heads up notifications
+AndroidNotificationChannel channel;
+
+/// Initialize the [FlutterLocalNotificationsPlugin] package.
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // getCurrentLocation();
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await Firebase.initializeApp();
+
+  // // Set the background messaging handler early on, as a named top-level function
+  // //  platform1 = const MethodChannel('flutter.native/helper');
+  if (!kIsWeb) {
+    channel = const AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      'This channel is used for important notifications.', // description
+      importance: Importance.high,
+    );
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    //App is in the foreground
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message, app is in the foreground!');
+      print('Message data: ${message.data}');
+
+      // if (message.notification != null) {
+      //   print('Message also contained a notification: ${message.notification}');
+      // }
+    });
+
+    /// Create an Android Notification Channel.
+    ///
+    /// We use this channel in the `AndroidManifest.xml` file to override the
+    /// default FCM channel to enable heads up notifications.
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    /// Update the iOS foreground notification presentation options to allow
+    /// heads up notifications.
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    )
+        .then((value) {
+      print("value:print");
+    });
+  }
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+Future<void> responseLockApp() async {
+  const platform1 = const MethodChannel('flutter.native/helper');
+
+  try {
+    await platform1.invokeMethod("lockScreen");
+  } on PlatformException catch (e) {
+    var s = "Failed to Invoke: '${e.message}'.";
+    print(s);
+  }
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  AppLifecycleState _lastLifecyleState;
+
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
+
+  @override
+  initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    Firebase.initializeApp();
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage message) {
+      if (message != null) {
+        print("hello Notification");
+      }
+    });
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            message.data["title"],
+            message.data["body"],
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channel.description,
+                // TODO add a proper drawable resource to android, for now using
+                //      one that already exists in example app.
+                icon: '@mipmap/ic_launcher',
+              ),
+            ));
+      }
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      // print('A new onMessageOpenedApp event was published!');
+      // if(message.data['title']=="Lock"){
+      //   responseLockApp();
+      // }else if(message.data['title']=="Location"){
+      //   getCurrentLocation();
+      // }else if(message.data['title']=="Flash"){
+      //   Flashlight.lightOn();
+      // }else if(message.data['title']=="EraseData"){
+      //
+      // }else if(message.data['title']=="Vibrate"){
+      //   Vibration.vibrate(duration: 10000,
+      //   );
+      // }else{
+      //   print("something when wrong");
+      // }
+    });
+  }
+
+  showNotification(Map<String, dynamic> msg) async {
+    var android = new AndroidNotificationDetails(
+      msg['title'],
+      msg['body'],
+      "channelDescription",
+      importance: Importance.high,
+      fullScreenIntent: true,
+    );
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android: android, iOS: iOS);
+    await flutterLocalNotificationsPlugin.show(
+        0, msg['title'], msg['body'], platform);
+  }
+
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print("hellooooo");
+    print(
+        "LifecycleWatcherState#didChangeAppLifecycleState state=${state.toString()}");
+    setState(() {
+      _lastLifecyleState = state;
+      print(_lastLifecyleState);
+    });
+  }
+
+  Future<void> responseNoti() async {
+    const platform1 = const MethodChannel('flutter.native/helper');
+
+    try {
+     await platform1.invokeMethod("initializeService");
+    } on PlatformException catch (e) {
+      print( "Failed to Invoke: '${e.message}'.");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("PUSH RECEIVED build context");
+      if (message.data['title'] == "Lock") {
+        responseLockApp();
+      } else if (message.data['title'] == "Location") {
+        // getCurrentLocation();
+      } else if (message.data['title'] == "Flash") {
+        Flashlight.lightOn();
+      } else if (message.data['title'] == "EraseData") {
+      } else if (message.data['title'] == "Vibrate") {
+        Vibration.vibrate(
+          duration: 10000,
+        );
+      } else {
+        print("something when wrong");
+      }
+      // responseLockApp();
+      // getCurrentLocation();
+      // bFirebaseMessaging.showPush(message);
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      if (message.data['title'] == "Lock") {
+        responseLockApp();
+      } else if (message.data['title'] == "Location") {
+        // getCurrentLocation();
+      } else if (message.data['title'] == "Flash") {
+        Flashlight.lightOn();
+      } else if (message.data['title'] == "EraseData") {
+      } else if (message.data['title'] == "Vibrate") {
+        Vibration.vibrate(
+          duration: 10000,
+        );
+      } else {
+        print("something when wrong");
+      }
+    });
+    FirebaseMessaging.onBackgroundMessage(_throwGetMessage);
+    return MaterialApp(
+      title: 'Find Lost Phone',
+      theme: ThemeData(
+        backgroundColor: Colors.white,
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreens(),
+    );
+  }
+// Crude counter to make messages unique
+  int _messageCount = 0;
+  /// The API endpoint here accepts a raw FCM payload for demonstration purposes.
+  String constructFCMPayload(String token) {
+    _messageCount++;
+    return jsonEncode({
+      'token': token,
+      'data': {
+        'via': 'FlutterFire Cloud Messaging!!!',
+        'count': _messageCount.toString(),
+      },
+      'notification': {
+        'title': 'Hello FlutterFire!',
+        'body': 'This notification (#$_messageCount) was created via FCM!',
+      },
+    });
+  }
+}
